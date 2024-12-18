bdda648e48684d0de2ddd8b3baacc97aa466e61b
Pyla, L. (Leo Nicolas, Student X-TCS) <62517091+Lpyla@users.noreply.github.com>
2024-12-18 17:28:30 +0100

    lateCommit hehe
    Added venv -- run ./Scripts/activate from privileged user shell
    venv should contain all dependencies

---
+++ connect_insert_mongodb.py
@@ -0,0 +1,38 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from datetime import datetime
+
+uri = "mongodb+srv://admin:CIT_admin0@citcluster0.mhrd1.mongodb.net/?retryWrites=true&w=majority&appName=CITcluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+database_name="CITcluster0"
+collection_name="Collection_1"
+db = client[database_name]
+collection = db[collection_name]
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
+    
+
+file_path="req.txt"
+manual_title="fichier1"
+
+with open(file_path, "r") as file:
+    for line in file:
+        content = line.strip()  # Remove leading and trailing whitespace
+        if content:  # Only process non-empty lines
+            document = {
+                "title": manual_title,
+                "content": content,
+                "timestamp": datetime.utcnow()  # Automatically generate a timestamp
+            }
+            collection.insert_one(document)  # Insert into MongoDB
+
+print("File data successfully pushed to MongoDB!")
